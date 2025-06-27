60d2b7cd4cb98ac6143e130b0b142ca0e23e717f
fitra2004 <nofriaidilfitra@gmail.com>
2025-06-27 15:34:47 +0700

    Add files via upload

---
+++ Praktikum26062025/src/praktikum26062025/Praktikum26062025.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum26062025;
+
+/**
+ *
+ * @author nofri
+ */
+public class Praktikum26062025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
